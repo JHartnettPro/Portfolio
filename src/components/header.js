950595af8a6127a1cd3266dde324b6440fc88ea5
JHartnettPro <JHartnettPro@gmail.com>
2020-10-07 17:53:12 -0500

    code

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab, faGithub } from '@fortawesome/free-brands-svg-icons'
 
-
-
-
-library.add(fab, fas, far, faGithub);
 
 
 
