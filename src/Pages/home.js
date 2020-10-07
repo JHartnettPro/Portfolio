5d76eaa0af386a3c1a685a9c97316d642356e86a
JHartnettPro <JHartnettPro@gmail.com>
2020-10-07 17:29:33 -0500

    make it better

---
+++ src/Pages/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+import Header from '../components/header';
+
+class Home extends Component{
+    render(){
+        return(
+            
+            <div >
+            <Header />
+            
+            </div>
+            
+        )
+    }
+} export default Home;
---
+++ src/components/header.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+
+
+
+library.add(fab, fas, far, faGithub);
+
+
+
+
+class Header extends Component {
+    render() {
+        return (
+            
+            <html>
+                <head><link href="css/hover.css" rel="stylesheet" media="all"></link></head>
+                
+            
+                
+               
+                
+                
+            
+            <div class= "all">
+            <body class="footer text-center ">
+  
+            <div id="fly-in">  
+<div class="text-black">John Hartnett<span></span></div>
+<div class="text-black">Full-stack <span>Web Developer</span></div>
+<div class="text-black">1000+ Hours<span>Experience</span></div>
+<div class="text-black"><h1 class="fix"><i class="fab fa-react "></i> C# <i class="fab fa-python "></i></h1> <span>proficiency</span></div>
+
+<div class="text-black">Available for Hire</div>
+
+
+</div>
+            
+                <div class="row justify-content-center ">
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-black">My Projects</h4>
+                        <br/>
+                        <a class="hvr-wobble-to-bottom-right" href="https://github.com/JHartnettPro/Python-Dating-App">Dating App</a>
+                        <br/>
+                        <a class="hvr-wobble-to-bottom-right" href="https://github.com/JHartnettPro/C-Hobby-Planner">Hobby Planner</a>
+                        <br/>
+                        <a  class="hvr-wobble-to-bottom-right" href="https://github.com/JHartnettPro/React-Speech-to-Text">Speech to Text</a>
+                    </div>
+                    
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase text-black text-center  mb-80">About Me</h4>
+                        <br/>
+                        <p class="lead mb-0 text-center text-black font-weight-bold">
+                                Full stack developer with a passion for creating functional and stylish web applications. 
+                                Looking to join a team of devoted professional developers to create user-friendly web applications and products. 
+                                 
+                        </p>
+                    </div>
+                </div>
+            
+            <div class="row justify-content-center ">
+                
+            <div class="col-sm-4 mb-5 mb-sm-0 ">
+                
+            <br/>
+                        <br/>
+            
+                        <h6 class="text-uppercase mb-4 text-black font-weight-bold">Contact</h6>
+                        <a class="btn btn-outline-light btn-social mx-1  " href="https://github.com/JHartnettPro"><i class="fab fa-fw fa-github"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1 " href="https://mail.google.com/mail/?view=cm&fs=1&to=jhartnettpro@gmail.com&su=Hi I'd like to hire you immediately&body=You can also call or text me anytime at 708 546 7062 for a quicker response."><i class="fas fa-envelope-square"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1 " href="https://linkedin.com/in/john-hartnett-24b2261b7/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        
+
+                        
+                    </div>
+                    </div>
+                    
+                   </body>
+                   </div>
+                   
+                   
+        
+        </html>
+
+        )
+    }
+}
+export default Header;
